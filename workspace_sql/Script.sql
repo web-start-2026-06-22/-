ef403560674b501675f8d89fe7ba0d1eb3f392ba
yongjun <lyj578520@naver.com>
2026-08-08 18:39:00 +0900

    20260808 금요일 수업내용 커밋

--- workspace_sql/Script.sql
+++ workspace_sql/Script.sql
@@ -11,6 +11,13 @@ from
 select
 	*
 from
+	emp
+where
+	1 != 1
+
+select
+	*
+from
 	dept;
 
 select
@@ -264,6 +271,39 @@ where
 select
 	*
 from
+	emp;
+
+select
+	*
+from
+	emp
+where
+	comm = null;
+
+select
+	*
+from
+	emp
+where
+	comm < 100;
+
+select
+	*
+from
+	emp
+where
+	comm is null;
+
+select
+	*
+from
+	emp
+where
+	comm is not null;
+
+select
+	*
+from
 	emp
 order by
 	sal;
@@ -367,6 +407,668 @@ where
 order by
 	sal asc,
 	ename desc;
+
+-- 집계 함수
+select
+	count(ename)
+from
+	emp;
+
+select
+	count(mgr)
+from
+	emp;
+
+select
+	count(comm)
+from
+	emp;
+
+select
+	count(*) '데이터 건수'
+from
+	emp;
+
+select
+	max(sal)
+from
+	emp;
+
+select
+	min(sal)
+from
+	emp;
+
+select
+	sum(sal)
+from
+	emp;
+
+select
+	avg(sal)
+from
+	emp;
+
+select
+	count(*),
+	max(sal),
+	min(sal),
+	sum(sal),
+	avg(sal)
+from
+	emp;
+
+select
+	length(ename),
+	ename
+from
+	emp;
+
+-- 집계 함수와 함께 컬럼을 select해오면 원하는 결과가 나오지 않음.
+-- 집계 함수가 셀 병합처럼 동작하기 때문에.
+select
+	count(*),
+	ename
+from
+	emp;
+
+select
+	*
+from
+	emp
+where
+	length(ename) = 4;
+
+-- 대상의 몇 번째부터 몇 개를 잘라오기
+select
+	substring(ename, 2, 3),
+	ename
+from
+	emp;
+
+select
+	substr(ename, 2, 3),
+	ename
+from
+	emp;
+
+-- 전부 교체
+select
+	replace(ename, 'A', '에이'),
+	ename
+from
+	emp;
+
+-- 대상의 자릿수를 맞춰주고 남으면 채워줌.
+select
+	lpad(ename, 10, '#')
+from
+	emp;
+
+select
+	lpad(ename, 3, '#')
+from
+	emp;
+
+select
+	rpad(ename, 10, '#')
+from
+	emp;
+
+select
+	trim('   a  b  c   ');
+
+select
+	concat(ename, job)
+from
+	emp;
+
+select
+	concat(ename, ' ', job)
+from
+	emp;
+
+-- 오라클에서 합치기 ename || job으로 사용 가능
+
+select
+	concat_ws('-', ename, job, empno)
+from
+	emp;
+
+-- 소수점 몇 째자리까지 살릴 것인지, 콤마 뒤에 표현
+-- 반올림
+select
+	round(3.14);
+
+select
+	round(3.15, 1);
+
+-- 올림
+select
+	ceil(3.14);
+
+select
+	ceil(-3.14);
+
+-- 내림
+select
+	floor(3.14);
+
+select
+	floor(-3.14);
+
+-- 버림
+select
+	truncate(-3.14, 1);
+
+-- 나머지
+select
+	mod(10, 3);
+
+-- 현재 시간
+select
+	now();
+
+select
+	sysdate();
+
+-- 날짜 출력 양식 지정
+select
+	date_format(now(), '%Y년 %m월 %d일 %H시 %i분 %s초')
+	
+-- 문자를 날짜 형으로 변환
+select
+	str_to_date('2026-08-07', '%Y-%m-%d');
+
+select
+	ifnull(comm, 0),
+	comm
+from
+	emp;
+
+select
+	coalesce(comm, 0),
+	comm
+from
+	emp;
+
+select
+	sal * 12 + comm
+from
+	emp;
+
+select
+	sal * 12 + ifnull(comm, 0)
+from
+	emp;
+
+-- 문제
+-- ename의 앞 두 글자만 출력
+
+select
+	lpad(ename, 2)
+from
+	emp;
+
+-- ename의 앞 두 글자만 원본 그대로 출력하고
+-- 4개의 *를 붙여서 출력
+-- SM****
+
+select
+	concat(lpad(ename, 2), '****')
+from
+	emp;
+
+select
+	rpad(substring(ename, 1, 2), 6, '*')
+from
+	emp;
+
+-- ename의 앞 두 글자만 원본 그대로 출력하고
+-- 나머지 이름 만큼의 * 출력
+-- WARD >> WA**, SMITH >> SM***
+
+select
+	rpad(substring(ename, 1, 2), length(ename), '*')
+from
+	emp;
+
+-- case 문
+select
+	*
+from
+	emp;
+
+select
+	job,
+	sal,
+	case
+		job
+		when 'CLERK' then sal * 1.05
+		when 'SALESMAN' then sal * 1.03
+		else sal * 1
+	end as upsal
+from
+	emp;
+
+select
+	job,
+	sal,
+	case
+		when job = 'CLERK' then sal * 1.05
+		when job = 'SALESMAN' then sal * 1.03
+		else sal * 1
+	end as upsal
+from
+	emp;
+
+select
+	sal,
+	comm,
+	case
+		when comm is null then 0
+		else comm
+	end
+from
+	emp;
+
+select
+	deptno
+from
+	emp
+group by
+	deptno;
+
+select
+	deptno,
+	count(*),
+	sum(sal)
+from
+	emp
+group by
+	deptno;
+
+select
+	deptno,
+	job,
+	count(*)
+from
+	emp
+group by
+	deptno,
+	job;
+
+select
+	deptno,
+	job
+from
+	emp
+where
+	deptno = 10
+group by
+	deptno,
+	job
+
+	
+select
+	deptno,
+	job
+from
+	emp
+where
+	deptno = 10
+group by
+	deptno,
+	job
+order by
+	job;
+
+select
+	avg(sal)
+from
+	emp;
+
+-- select
+-- 	ename,
+-- 	sal,
+-- 	avg(sal)
+-- from
+-- 	emp
+-- where
+-- 	sal >= avg(sal);
+
+select
+	avg(sal) as avg_sals,
+	deptno,
+	job
+from
+	emp
+where
+	deptno = 10
+group by
+	deptno,
+	job
+having
+	deptno = 10;
+
+
+-- 직업 별로 연봉 1000 이상인 사람이 3명 이상인 경우만 출력
+select
+	job,
+	count(*) cnt
+from
+	emp
+where
+	sal >= 1000
+	-- 	and cnt >= 3
+	-- 	and count(*) >= 3
+group by
+	job
+having
+	count(*) >= 3;
+
+select
+	job,
+	1 as num
+from
+	emp
+where
+	sal > 1000
+group by
+	job
+having
+	count(*) >= 3
+order by
+	job desc;
+
+select
+	*
+from
+	emp
+where
+	deptno = 10
+union
+select
+	*
+from
+	emp
+where
+	deptno = 10;
+
+select
+	*
+from
+	emp
+where
+	deptno = 10
+union all
+select
+	*
+from
+	emp
+where
+	deptno = 10;
+
+select
+	*
+from
+	emp
+where
+	sal > (
+	select
+		sal
+	from
+		emp
+	where
+		ename = 'WARD'
+);
+
+select * from emp
+where sal > (select avg(sal) from emp);
+
+-- 'WARD'의 연봉만 출력
+
+select
+	sal
+from
+	emp
+where
+	ename = 'WARD';
+
+-- 부서 별 최고 연봉자
+-- 1. 부서 별 최고 연봉
+
+select
+	deptno,
+	max(sal)
+from
+	emp
+group by
+	deptno;
+
+select
+	ename,
+	sal
+from
+	emp
+	-- where
+	-- 	sal = 3000
+	-- 	or sal = 2850
+	-- 	or sal = 5000;
+where
+	sal in (3000, 2850, 5000);
+
+select
+	ename,
+	sal,
+	deptno
+from
+	emp
+where
+	sal in (
+	select
+		max(sal)
+	from
+		emp
+	group by
+		deptno
+);
+
+select * from dept;
+
+select
+	*
+from
+	emp,
+	dept;
+
+select
+	*
+from
+	emp,
+	dept
+where
+	emp.deptno = dept.deptno;
+
+select
+	e.empno,
+	e.ename,
+	e.mgr,
+	m.ename
+from
+	emp as e
+join emp as m on
+	e.mgr = m.empno;
+
+select
+	*
+from
+	emp e,
+	dept d
+where
+	e.deptno = d.deptno;
+
+-- select
+	-- 	ename,
+	-- 	dname,
+	-- 	deptno
+	-- from
+	-- 	emp e,
+	-- 	dept d
+	-- where
+	-- 	e.deptno = d.deptno;
+
+select
+	e.ename,
+	d.dname,
+	d.deptno
+from
+	emp e,
+	dept d
+where
+	e.deptno = d.deptno;
+
+select
+	*
+from
+	salgrade;
+
+select
+	e.ename,
+	e.sal,
+	s.grade,
+	s.losal,
+	s.hisal
+from
+	emp e,
+	salgrade s
+where
+	(e.sal between s.losal and hisal)
+	and e.ename = 'SMITH';
+
+select * from emp;
+
+
+-- mgr이 null인 것은 빠졌다.
+select
+	e.empno,
+	e.ename,
+	e.mgr '상사 사번',
+	m.ename 상사
+from
+	emp as e
+join emp as m on
+	e.mgr = m.empno;
+-- where
+-- 	e.ename = 'SMITH';
+
+-- 문제
+-- 이름, 급여, 부서명, 급여 등급, 등급 순 내림차순
+
+select
+	e.ename 사원명,
+	e.sal 급여,
+	d.dname 부서명,
+	s.grade 급여등급
+from
+	emp e
+join dept d on
+	e.deptno = d.deptno,
+	salgrade s
+where
+	e.sal between s.losal and s.hisal
+order by
+	s.grade desc,
+	e.sal desc;
+
+select
+	*
+from
+	emp e
+join dept d on
+	(e.deptno = d.deptno);
+
+select
+	*
+from
+	emp e
+join dept d
+		using(deptno);
+
+select
+	e.empno,
+	e.ename,
+	e.mgr '상사 사번',
+	m.ename 상사
+from
+	emp as e
+left outer join emp as m on
+	e.mgr = m.empno;
+
+select
+	e.empno,
+	e.ename,
+	e.mgr '상사 사번',
+	m.ename 상사
+from
+	emp as e
+right outer join emp as m on
+	e.mgr = m.empno;
+
+select
+	*
+from
+	dept;
+
+select
+	sal
+from
+	emp
+where
+	ename = 'SCOTT';
+
+select
+	grade
+from
+	salgrade
+where
+	3000 between losal and hisal;
+
+select
+	sal,
+	ename,
+	(
+	select
+		grade
+	from
+		salgrade
+	where
+		3000 between losal and hisal) grade
+from
+	emp
+where
+	ename = 'SCOTT';
+
+
+
+-- 문제
+-- deptno, dname, empno, ename
+-- 모든 부서가 다 나오게 출력
+-- 부서 번호 오름차순, 이름 오름차순
+
+select
+	d.deptno,
+	d.dname,
+	e.empno,
+	e.ename
+from
+	dept d
+left outer join emp e on
+	(d.deptno = e.deptno)
+order by
+	d.deptno asc,
+	e.ename asc;
+
 
 -- select
 -- 	ename + sal
